992584135d65911df3065a8f0fe9e747c0a9ebb3
Marc-Antoine Parent <maparent@acm.org>
2015-04-28 14:39:52 -0400

    fix migration

--- assembl/alembic/versions/2df3bdfbc594_facebook_update_fields.py
+++ assembl/alembic/versions/2df3bdfbc594_facebook_update_fields.py
@@ -1,14 +1,14 @@
 """facebook_update_fields
 
 Revision ID: 2df3bdfbc594
-Revises: 599eb2e48439
+Revises: 475125b7bfe3
 Create Date: 2015-03-27 19:07:06.046621
 
 """
 
 # revision identifiers, used by Alembic.
 revision = '2df3bdfbc594'
-down_revision = '63cb39048ef'
+down_revision = '475125b7bfe3'
 
 from alembic import context, op
 import sqlalchemy as sa
